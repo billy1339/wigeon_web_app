cbad8e229ba738210929e133b51166c1bf67bffb
Billy1339 <william.m.sargent@gmail.com>
2016-11-15 22:12:17 -0500

    styling and filer stuff

--- scripts/filters/cut.filter.js
+++ scripts/filters/cut.filter.js
@@ -6,7 +6,6 @@ var CutFilter =  function() {
             max = parseInt(max, 10);
             if (!max) return value;
             if (value.length <= max) return value;
-
             value = value.substr(0, max);
             if (wordwise) {
                 var lastspace = value.lastIndexOf(' ');
