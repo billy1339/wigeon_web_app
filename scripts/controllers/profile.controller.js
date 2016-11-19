f8272828315e8817ca3b1bdb3718abd9a36a6989
Elizabeth Sargent <esargent@ESargent-JWP.local>
2016-11-19 14:52:56 -0500

    profile controller and template

---
+++ scripts/controllers/profile.controller.js
@@ -0,0 +1,125 @@
+var ProfileCtrl = function($scope, $http, $cookies, $window, SuggestionFactory, $rootScope, $sce, YelpFactory) {
+  'use strict'
+
+  // we want to get all the user info right off the back and probably have deferred promise
+  getUserInfo();
+  initListeners();
+
+  $scope.view = "LIST";
+  $scope.quantity = 20; 
+  function getUserInfo() {
+  	var user_token = $cookies.get("wigeon_user_token");
+  	if (!user_token) {
+  		$window.location.href = '/#/';
+  	}
+  	PopulateSuggestions();
+  }
+
+  function PopulateSuggestions() {
+    var promise = SuggestionFactory.fetch(1, 0);
+    promise.then(function(response) {
+      $scope.suggestions = response; 
+      var secondPromise = SuggestionFactory.fetch(10, 1);
+      secondPromise.then(function(response) {
+        $scope.suggestions = $scope.suggestions.concat(response);
+      });
+    });
+  }
+
+  $scope.populateModal = function(id) {
+    var sug = findSuggestion(id);
+    $scope.suggestionModelInfo = sug;
+    if (sug.suggestion_type.title === "GO") {
+      var promise = YelpFactory.fetch(sug.suggestion_source_item_id); 
+      promise.then(function(response) {
+        console.log(response); 
+        $scope.yelp = response; 
+        $("#SuggestionDetailModal").modal();
+      });
+    } else {
+      $("#SuggestionDetailModal").modal();
+    };
+  }
+
+  function findSuggestion(id) {
+    var sugs, item;
+    sugs = $scope.suggestions;
+    for(var i=0; i < sugs.length; i++) {
+      if (id == sugs[i].inbox_suggestion.suggestion_id) {
+        item = sugs[i].inbox_suggestion;
+        break;
+      }
+    }
+    return item;
+  }
+
+  $scope.getIframeSrc = function(src) {
+    return $sce.trustAsResourceUrl('https://www.youtube.com/embed/' + src);
+  };
+
+  $scope.returnTrustedSrc = function(src) {
+    src = src + "?cb=" + new Date().getTime();
+    return $sce.trustAsResourceUrl(src);
+  };
+
+  $scope.changeLayout = function(layout) {
+    $scope.view = layout;
+  };
+
+  function initListeners() {
+    $(document).ready(function() {
+      $("#NestCtrl").on('hidden.bs.modal', '#SuggestionDetailModal', function () {
+        var audio = $("#MusicPreview")[0];
+        if (audio !== undefined) {
+          audio.pause(); 
+        } 
+      });
+    });
+  }
+
+  $scope.seeMore = function() {
+    $scope.quantity += 20; 
+  }
+
+
+
+// inbox_suggestion
+// suggestion_address
+// suggestion_background_image
+// suggestion_content_author
+// suggestion_creation_epoch
+// suggestion_creator_user_id
+// suggestion_description
+// suggestion_general_link
+// suggestion_icon_image
+// suggestion_id
+// suggestion_in_inbox
+// suggestion_is_deleted
+// suggestion_is_private
+// suggestion_is_self_suggestion
+// suggestion_is_starred
+// suggestion_last_comment
+// suggestion_media_string
+// suggestion_num_comments
+// suggestion_num_stars
+// suggestion_parent_suggestion_id
+// suggestion_secondary_source_link
+// suggestion_source
+// suggestion_source_item_id
+// suggestion_source_media_link
+// suggestion_source_preview_link
+// suggestion_subtype_string
+// suggestion_tags
+// suggestion_title
+// suggestion_type
+// suggestion_url
+
+  $scope.SignOut = function() {
+    $cookies.remove("wigeon_user_token");
+    $window.location.href = '/#/';
+  }
+};
+
+NestCtrl.$inject = ['$scope', '$http', '$cookies', '$window', 'SuggestionFactory', '$rootScope', '$sce', 'YelpFactory'];
+angular.module('WigeonApp').controller('ProfileCtrl', ProfileCtrl);
+
